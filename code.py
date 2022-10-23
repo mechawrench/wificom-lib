3819ac5c4b00a4d5387fbe09b298efe3b43b0e9c
Jesse Schneider <mechawrench@users.noreply.github.com>
2022-10-23 10:11:38 -0700

    Feature/fix rp2040 connect wifi (#23)
    * Bring RP2040 Connect wifi current with mqtt ccall
    
    * Fix pylint errors

--- code.py
+++ code.py
@@ -3,16 +3,10 @@ This file is part of the DMComm project by BladeSabre. License: MIT.
 WiFiCom on supported boards (see board_config.py).
 '''
 import time
-import board
 # import busio
 import digitalio
 import pwmio
 import usb_cdc
-
-import board_config
-
-# Blink LED while starting up. Doing this here because the following imports are slow.
-led = pwmio.PWMOut(board_config.led_pin, duty_cycle=0x8000, frequency=1, variable_frequency=True)
 
 # pylint: disable=wrong-import-position
 from dmcomm import CommandError, ReceiveError
@@ -21,7 +15,11 @@ import dmcomm.protocol
 import dmcomm.protocol.auto
 import dmcomm.protocol.realtime as rt
 from wificom.mqtt import platform_io
-# pylint: enable=wrong-import-position
+
+import board_config
+
+# Blink LED while starting up. Doing this here because the following imports are slow.
+led = pwmio.PWMOut(board_config.led_pin, duty_cycle=0x8000, frequency=1, variable_frequency=True)
 
 LED_DUTY_CYCLE_DIM=0x1000 # pylint: disable=invalid-name
 
