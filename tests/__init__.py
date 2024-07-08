e0eb587022e282fdc2a6d13b24537bf2a5ce9adf
BladeSabre <bladethecoder@gmail.com>
2024-07-08 01:32:33 +0100

    Oops

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,6 +2,6 @@
 import os
 import sys
 
-root_dir = os.path.dirname(os.path.dirname(os.path.realpath("__file__")))
+root_dir = os.path.dirname(os.path.realpath("__file__"))
 lib_dir = os.path.join(root_dir, "lib")
 sys.path.append(lib_dir)
